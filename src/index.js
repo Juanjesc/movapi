da57a5c27aa9f7abc7eee799618fdade11d7fa79
juanjesc <juan97jesus@hotmail.com>
2022-06-18 18:23:25 +0200

    2ºcommit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faLaughBeam } from '@fortawesome/free-solid-svg-icons'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
 
   <App />
+  <div className="creditos">
+        <p>By: Juan Jesús Sedano  
+        <span> 
+         <FontAwesomeIcon icon={faLaughBeam}/>
+        </span>
+        </p>
+      </div>
+  </div>
 
 );
 
